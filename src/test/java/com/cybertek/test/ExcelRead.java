58faf1194c43da96c59b3b8f7150fc1f0a86c8be
Zuura Idrisova <zuuraidrisova@gmail.com>
2020-11-29 20:03:01 -0500

    Junit review

---
+++ src/test/java/com/cybertek/test/ExcelRead.java
@@ -0,0 +1,17 @@
+package com.cybertek.test;
+
+import org.junit.Test;
+
+import java.io.File;
+
+public class ExcelRead {
+
+    @Test
+    public void excelRead_test(){
+
+        File file = new File("VytrackTestUsers.xlsx");
+
+        
+
+    }
+}
